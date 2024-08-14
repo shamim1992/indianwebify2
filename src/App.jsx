a2b623236d200d285a74eaa497884bc4aa26ff13
Shamim Sheikh <shamimsheikh@Shamims-MacBook.local>
2024-08-14 22:09:38 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 // npm install framer-motion
