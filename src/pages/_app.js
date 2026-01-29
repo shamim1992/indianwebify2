adfd757dea5c47f16e0fa5d03160fa3c8b243969
shamim1992 <shamimakhtarsheikh@gmail.com>
2026-01-29 16:00:47 +0530

    updated

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,10 +8,8 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         {/* Default Favicon */}
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" href="/logo2.png" />
+        <link rel="apple-touch-icon" href="/logo2.png" />
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <main className='font-sans'>
